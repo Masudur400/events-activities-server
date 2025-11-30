a5eee9d246559246b630da49d1448c8cf1a5d390
Masudur400 <md8004210@gmail.com>
2025-11-30 21:58:14 +0600

    file setup

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors' 
+// import passport from 'passport'
+// import './app/config/passport'
+// import expressSession from 'express-session'
+// import { envVars } from './app/config/env'
+import cookieParser from 'cookie-parser' 
+import { envVars } from './app/config/env'
+
+
+const app = express()
+// app.use(expressSession({
+//     secret: env.EXPRESS_SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+app.use(cookieParser())
+app.use(express.json())
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: [
+        envVars.FRONTEND_URL,
+    ],
+    credentials: true,
+    //     methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
+    //   allowedHeaders: ["Content-Type", "Authorization"]
+})) 
+
+
+
+// app.use('/api', router)
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'welcome to events & activities...!'
+    })
+})
+
+// app.use(globalErrorHandler)
+// app.use(notFound)
+
+
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app'; 
+import { envVars } from './app/config/env';
+// import { seedSuperAdmin } from './app/utils/seedSupperAdmin';
+
+
+let server: Server 
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envVars.DB_URL)
+        console.log('Connect to DB...!')
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening to port ${envVars.PORT}`) 
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+ 
+
+
+(async () => {
+    // await connectRedis()
+    await startServer()
+    // await seedSuperAdmin()
+})()
+
+
+
+// 🧠 কেন এগুলো ব্যবহার করা হয়?
+// 👉 যাতে server হঠাৎ করে crash না করে, বরং একটা clean shutdown হয়।
+// 👉 যাতে কোনো unexpected error এও আমরা log রেখে server বন্ধ করতে পারি।
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal recieved. server shutting down....!');
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+process.on('SIGINT', () => {
+    console.log('SIGINT signal recieved. server shutting down....!');
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+process.on('unhandledRejection', (err) => {
+    console.log('unhandled rejection detected. server shutting down....!', err);
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+process.on('uncaughtException', (err) => {
+    console.log('uncaught exception detected. server shutting down....!', err);
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
